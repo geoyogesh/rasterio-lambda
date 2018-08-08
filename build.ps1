f07ebb45fb831a055ae16a344d3d95908c92f4ca
Yogesh Dhanapal <geoyogesh@gmail.com>
2018-08-07 23:35:49 -0400

    cleanup

--- build.ps1
+++ build.ps1
@@ -2,4 +2,4 @@ docker build --rm -t rasterio-lambda .
 $env:DOCKER_ID_USER = "geoyogesh"
 docker login
 docker tag rasterio-lambda $env:DOCKER_ID_USER/rasterio-lambda
-docker push $env:DOCKER_ID_USER/rasterio-lambda
+#docker push $env:DOCKER_ID_USER/rasterio-lambda
